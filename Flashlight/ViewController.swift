d50598ca50d1ef0a982a5f908de00bba4a959301
FaiChou <faichou.zh@gmail.com>
2024-11-28 20:49:30 +0800

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by 周辉 on 2024/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
